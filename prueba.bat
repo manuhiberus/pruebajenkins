16994aa85f1c51aa0bd6c6a1b504fe97ad9fdb7e
manuhiberus <msanchez@hiberus.com>
2018-04-05 13:11:30 +0200

    Update prueba.bat

--- prueba.bat
+++ prueba.bat
@@ -1,6 +1,7 @@
 set PATH=C:\Program Files\Java\jdk1.7.0_141\bin;C:\apache-ant-1.9.1\bin;%PATH%
 set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_141
-set ANT_HOME=C:\apache-ant-1.9.1
+set ANT_HOME=C:\apache-ant-1.7.1
 set ANT_OPTS=-Xmx768m -XX:MaxPermSize=256m
 set antt= ant -version
+set javaa = java -version
 cmd
